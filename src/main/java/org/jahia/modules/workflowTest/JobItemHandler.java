33861a4c0f5c4840b475f05f7e075512ca3a3bb0
tdraier <tdraier@jahia.com>
2019-02-19 11:03:23 +0100

    initial

---
+++ src/main/java/org/jahia/modules/workflowTest/JobItemHandler.java
@@ -0,0 +1,22 @@
+package org.jahia.modules.workflowTest;
+
+
+import org.jahia.services.workflow.jbpm.custom.AbstractWorkItemHandler;
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+public class JobItemHandler  extends AbstractWorkItemHandler implements WorkItemHandler {
+
+    @Override
+    public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+        System.out.println("xxxx");
+    }
+
+    @Override
+    public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+
+    }
+
+}
+
